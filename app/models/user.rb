79269cc76e38879e0d40521ca3cdcb09a40380d2
Paul Howard <paul@paul-howard.net>
2015-01-23 15:51:57 -0800

    Add required authentication validations to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,14 @@ class User < ActiveRecord::Base
   has_many :events
   has_many :locations
 
+  has_secure_password
+
+  validates_confirmation_of :password on :create
+  validates_presence_of :password_confirmation
+
+  validates :email,
+    presence: true,
+    uniqueness: {case_sensitive: false}
+
+
 end
