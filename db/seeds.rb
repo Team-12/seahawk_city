403ece5ae4eb31882284212982d2da29915d7c91
Paul Howard <paul@paul-howard.net>
2015-01-24 09:05:09 -0800

    Add default users to db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,51 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Six Users
+users = User.create([
+          { user_name: "SaintPete",
+            email: "pete@seahawks.com",
+            password: "123",
+            password_confirmation: "123" 
+          },
+          { user_name: "JustRussell",
+            email: "russell@seahawks.com",
+            password: "123",
+            password_confirmation: "123" 
+          },
+          { user_name: "BeastMode",
+            email: "marshawn@seahawks.com",
+            password: "123",
+            password_confirmation: "123" 
+          },
+          { user_name: "OptimusPrime",
+            email: "rsherman@seahawks.com",
+            password: "123",
+            password_confirmation: "123" 
+          },
+          { user_name: "BamBamKam",
+            email: "kam@seahawks.com",
+            password: "123",
+            password_confirmation: "123" 
+          },
+          { user_name: "TheWillverine",
+            email: "luke@seahawks.com",
+            password: "123",
+            password_confirmation: "123" 
+          }
+        ])
+
+# Six Locations
+# locations = Location.create([
+#           {Space Needle},
+#           {Russell Investments Center},
+#           {Stadium Place Apartments},
+#           {Yoga Studio}
+#         ])
+
+# Six Events
+# events = Event.create([])
+
+# ?? Checkins
+# checkins = Checkin.create([])
