553933be25259061fad0c2d632ac3cd1126a2c2e
DEVDESK <swoolsey@devdesk.net>
2015-01-24 12:17:27 -0800

    added test table to locations#index to show locations in DB

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -11,6 +11,17 @@ class LocationsController < ApplicationController
     end
 
     def create
+        # @location = Location.find_or_create_by(latitude: :latitude,longitude: :longitude)
+        # @location = Location.where(location_params).where(location.latitude => :latitude, location.longitude => :longitude).first_or_create
+        existing_location = Location.find_by_latitude_and_longitude(params[:location][:latitude],params[:location][:longitude])
+        if existing_location
+            redirect_to locations_path
+        else
+            @location = Location.create(location_params)
+            redirect_to new_location_path
+        end
+
+        # redirect_to locations_path
     end
 
     def show
@@ -23,6 +34,10 @@ class LocationsController < ApplicationController
     end
 
     def destroy
+    end
+
+    def location_params
+        params.require(:location).permit(:name,:desc,:longitude,:latitude,:address_street,:address_city,:address_state,:address_zip,:address_country)
     end
 
 end
