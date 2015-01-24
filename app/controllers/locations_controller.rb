3e4992f8a04b52dcf6df577ea338d886483cccb8
DEVDESK <swoolsey@devdesk.net>
2015-01-23 19:02:03 -0800

    updated location controller and new location view

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,9 +1,13 @@
 class LocationsController < ApplicationController
+    # geocoded_by :address
+    # after_validation :geocode
 
     def index
+        @locations = Location.all
     end
 
     def new
+        @location = Location.new
     end
 
     def create
