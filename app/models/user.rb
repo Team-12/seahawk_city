4015fcde4471143ac74e24756b62ffc15ab18f2c
Paul Howard <paul@paul-howard.net>
2015-01-23 16:32:42 -0800

    Get user auth working in console

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,17 @@ class User < ActiveRecord::Base
   has_many :events
   has_many :locations
 
-  has_secure_password
-
-  validates_confirmation_of :password on :create
-  validates_presence_of :password_confirmation
-
   validates :email,
     presence: true,
     uniqueness: {case_sensitive: false}
 
+  has_secure_password
+
+  validates_confirmation_of :password, on: :create
+  validates_presence_of :password_confirmation
+
+  def self.authenticate email, password
+    User.find_by_email(email).try(:authenticate, password)
+  end
 
 end
