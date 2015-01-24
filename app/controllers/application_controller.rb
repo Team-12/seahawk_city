9dcd4a2ca81c57f9a612f712a0606fea964e620d
Paul Howard <paul@paul-howard.net>
2015-01-24 08:06:56 -0800

    Add basic UI for user signup/login/logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,22 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :current_user
+
+  def current_user
+    @current_user ||= User.find_by_id(session[:user_id])
+  end
+  
+  def is_authenticated?
+    if !current_user
+      flash[:danger]='You must be logged in to access that page.'
+      redirect_to login_path
+      false
+    else
+      true
+    end
+  end
+
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,35 @@
 class UsersController < ApplicationController
 
-    def new
-        #signup get
+  def new
+    @user = User.new
+  end
 
+  def create
+    @user = User.create(user_params)
+    if @user.errors.any?
+      render :new
+    else
+      flash[:success] = 'User has been created. You can now log in.'
+      redirect_to login_path
     end
+  end
 
-    def create
-        # signup post
-    end
+  def profile
+    # get my profile
+  end
 
-    def profile
-        # get my profile
-    end
+  def edit_profile
+    #edit my profile (get)
+  end
 
-    def edit_profile
-        #edit my profile (get)
-    end
+  def update_profile
+    #patch my profile (update)
+  end
 
-    def update_profile
-        #patch my profile (update)
-    end
+  ###
+  def user_params
+    params.require(:user).permit(:user_name,:email,:password,:password_confirmation)
+  end
 
 end
 
