630813bdd3a3f6b1af456d091e064a086a47375b
DEVDESK <swoolsey@devdesk.net>
2015-01-25 15:37:02 -0800

    still getting Geocoder address error-but have lat long to work with

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -19,19 +19,30 @@ class LocationsController < ApplicationController
             else
                 #IF it's not an existing location, create a new MapFlag and attach image
                 @lat_long_string = params[:location][:latitude]+", "+params[:location][:longitude]
-                return geocoded_address
+                # return reverse_geocoded_address
 
                 # The code below works and can add site to DB only if geocoded_by :address is commented out in models>location.rb
                 # Has something to do with this: http://www.rubydoc.info/github/alexreisner/geocoder/master/frames#Forward_and_Reverse_Geocoding_in_the_Same_Model
 
-                # @location = Location.create(location_params)
-                # redirect_to locations_path
+                @location = Location.create(reverse_geocoded_address)
+                redirect_to locations_path
 
             end
         else
             #GEOCODE BY ADDRESS to Get Longitude & Latitude
-            lat_long_array = Geocoder.coordinates(address)
-            render json: lat_long_array
+            @lat_long_array = Geocoder.coordinates(address)
+            latitude = @lat_long_array[0]
+            longitude = @lat_long_array[1]
+            existing_location = Location.find_by_latitude_and_longitude(latitude, longitude)
+            if existing_location
+                #IF it's an existing location, we should add photo to existing MapFlag
+                render json: existing_location
+            else
+                #IF it's not an existing location, create a new MapFlag and attach image
+                # render json: address_hash
+                @location = Location.create(address_hash)
+                redirect_to locations_path
+            end
         end
     end
 
@@ -61,20 +72,39 @@ class LocationsController < ApplicationController
         return [street, city, state, zip, country].compact.join(', ')
     end
 
-    def geocoded_address
+    def address_hash
+        form_entry_hash = {
+            "address_street" => params[:location][:address_street],
+            "address_city" => params[:location][:address_city],
+            "address_state" => params[:location][:address_state],
+            "address_zip" => params[:location][:address_zip],
+            "address_country" => params[:location][:address_country],
+            "latitude" => @lat_long_array[0]
+            # "longitude" => @lat_long_array[1]
+            #one or other of lat and long works, but with both it breaks...WHY???
+        }
+    end
+
+    def reverse_geocoded_address
         address_array = Geocoder.search(@lat_long_string)
         first_match = address_array.first
         parsed_address_hash = {
-            "street" => first_match.street_number+" "+first_match.route,
-            "city" => first_match.city,
-            "state" => first_match.state_code,
-            "zip" => first_match.postal_code,
-            "country" => first_match.country_code,
-            "country_full" => first_match.country,
-            "address" => first_match.address
+            "address_street" => first_match.street_number+" "+first_match.route,
+            "address_city" => first_match.city,
+            "address_state" => first_match.state_code,
+            "address_zip" => first_match.postal_code,
+            "address_country" => first_match.country_code
+
+            # TRIED BELOW FOR LAT/LONG... Needs fixing
+            # "latitude" => first_match.lat,
+            # "longitude" => first_match.lng
+            # "latitude" => params[:location][:latitude].to_f,
+            # "longitude" => params[:location][:longitude].to_f
+            # "country_full" => first_match.country,
+            # "address" => first_match.address
         }
-            # render json: street+", "+city+", "+state+", "+zip+", "+country
-        render json: parsed_address_hash
+        # render json: street+", "+city+", "+state+", "+zip+", "+country
+        # render json: parsed_address_hash
     end
 
 end
