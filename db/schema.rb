8d67d05c485355b37037041ef663a833ecf230f9
Paul Howard <paul@paul-howard.net>
2015-01-25 15:40:55 -0800

    Delete model 'test'; add user_id to 'event' model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150123015745) do
+ActiveRecord::Schema.define(version: 20150125073419) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20150123015745) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "photo_url"
+    t.integer  "user_id"
   end
 
   create_table "locations", force: :cascade do |t|
