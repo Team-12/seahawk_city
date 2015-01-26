9252825a00cad4cbe6b7027c79ba0ff6d5e5856e
Paul Howard <paul@paul-howard.net>
2015-01-26 10:53:02 -0800

    Change order of javascript requires to fix gmaps problem on Heroku deploy

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,10 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require gmaps/google
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
 //= require underscore
-//= require gmaps/google
 //= require_tree .
 
