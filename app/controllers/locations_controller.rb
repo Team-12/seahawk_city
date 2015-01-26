aeeebc1bbacb22d0ef5e616fcb413595e4dfbc52
DEVDESK <swoolsey@devdesk.net>
2015-01-26 12:31:00 -0800

    added search logice  for single string location lookup, cleaned up notes

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -15,45 +15,66 @@ class LocationsController < ApplicationController
 
 
     def create
-        if (params[:location][:latitude] != "") and (params[:location][:longitude] != "")
-            #IF there is not a Latitude or Longitude, geocode based on address below
-            existing_location = Location.find_by_latitude_and_longitude(params[:location][:latitude],params[:location][:longitude])
-            if existing_location
-                #IF it's an existing location, we should add photo to existing MapFlag
-                redirect_to new_location_path
-            else
-                #IF it's not an existing location, create a new MapFlag and attach image
-                @lat_long_string = params[:location][:latitude]+", "+params[:location][:longitude]
-                # return reverse_geocoded_address
-
-                # The code below works and can add site to DB only if geocoded_by :address is commented out in models>location.rb
-                # Has something to do with this: http://www.rubydoc.info/github/alexreisner/geocoder/master/frames#Forward_and_Reverse_Geocoding_in_the_Same_Model
-                @location = Location.create(reverse_geocoded_address)
-
-                # Returns all locations in the database within 1 mile of newly created location (first object in array is the newly created site itself)
-                nearby_locations = Location.near((@location), 1)
-                render json: nearby_locations
-                # redirect_to locations_path
-
-            end
-        else
+        #If Search Field is filled in...
+        if params[:location][:address]
             #GEOCODE BY ADDRESS to Get Longitude & Latitude
+            #AND REVERSE-GEOCODE on Result to keep data normalized
             @lat_long_array = Geocoder.coordinates(address)
-            latitude = @lat_long_array[0]
-            longitude = @lat_long_array[1]
+            latitude = @lat_long_array[0].to_s
+            longitude = @lat_long_array[1].to_s
+            @lat_long_string = latitude + ", " + longitude
             existing_location = Location.find_by_latitude_and_longitude(latitude, longitude)
-            if existing_location
-                #IF it's an existing location, we should add photo to existing MapFlag
-                render json: existing_location
-            else
-                # IF it's not an existing location, create a new MapFlag and attach image
-                # render json: address_hash
-                @location = Location.create(address_hash)
+                if existing_location
+                    #IF it's an existing location, we should add photo to existing MapFlag
+                    render json: existing_location
+                else
+                    #IF it's not an existing location, create a new MapFlag and attach image
+                    # render json: address_hash
+                    @location = Location.create(search_address_hash)
 
-                # Returns all locations in the database within 1 mile of newly created location (first object in array is the newly created site itself)
-                nearby_locations = Location.near((@location), 1)
-                render json: nearby_locations
-                # redirect_to locations_path
+                    #Returns all locations in the database within 1 mile of newly created location (first object in array is the newly created site itself)
+                    nearby_locations = Location.near((@location), 1)
+                    render json: nearby_locations
+                    # redirect_to locations_path
+                end
+        else
+            if (params[:location][:latitude] != "") and (params[:location][:longitude] != "")
+                #REVERSE-GEOCODE to Get Address Details
+                existing_location = Location.find_by_latitude_and_longitude(params[:location][:latitude],params[:location][:longitude])
+                if existing_location
+                    #IF it's an existing location, we should add photo to existing MapFlag
+                    redirect_to new_location_path
+                else
+                    #IF it's not an existing location, create a new MapFlag and attach image
+                    @lat_long_string = params[:location][:latitude]+", "+params[:location][:longitude]
+                    #Return reverse_geocoded_address
+                    @location = Location.create(reverse_geocoded_address)
+
+                    #Returns all locations in the database within 1 mile of newly created location (first object in array is the newly created site itself)
+                    nearby_locations = Location.near((@location), 1)
+                    render json: nearby_locations
+                    # redirect_to locations_path
+
+                end
+            else
+                #GEOCODE BY ADDRESS to Get Longitude & Latitude
+                @lat_long_array = Geocoder.coordinates(address)
+                latitude = @lat_long_array[0]
+                longitude = @lat_long_array[1]
+                existing_location = Location.find_by_latitude_and_longitude(latitude, longitude)
+                if existing_location
+                    #IF it's an existing location, we should add photo to existing MapFlag
+                    render json: existing_location
+                else
+                    # IF it's not an existing location, create a new MapFlag and attach image
+                    # render json: address_hash
+                    @location = Location.create(address_hash)
+
+                    # Returns all locations in the database within 1 mile of newly created location (first object in array is the newly created site itself)
+                    nearby_locations = Location.near((@location), 1)
+                    render json: nearby_locations
+                    # redirect_to locations_path
+                end
             end
         end
     end
@@ -80,8 +101,9 @@ class LocationsController < ApplicationController
         state = params[:location][:address_state]
         zip = params[:location][:address_zip]
         country = params[:location][:address_country]
-
-        return [street, city, state, zip, country].compact.join(', ')
+        address_search = params[:location][:address]
+        # .compact removes the nil values so string can concatenate properly
+        return [street, city, state, zip, country, address_search].compact.join(', ')
     end
 
     def address_hash
@@ -94,8 +116,31 @@ class LocationsController < ApplicationController
             "address_zip" => params[:location][:address_zip],
             "address_country" => params[:location][:address_country],
             "latitude" => @lat_long_array[0],
-            "longitude" => @lat_long_array[1]
-            #one or other of lat and long works, but with both it breaks...WHY???
+            "longitude" => @lat_long_array[1],
+            "address" => params[:location][:address]
+        }
+    end
+
+    def search_address_hash
+        address_array = Geocoder.search(@lat_long_string)
+        first_match = address_array.first
+        # We can use if statements to determine which value to add to database if fields are filled in that contradict with reverse-geocode results
+        if params[:location][:name] != ""
+            name_value = params[:location][:name]
+        else
+            name_value = params[:location][:address]
+        end
+        search_hash = {
+            "name" => name_value,
+            "desc" => params[:location][:desc],
+            "latitude" => @lat_long_array[0],
+            "longitude" => @lat_long_array[1],
+            "address_street" => first_match.street_number+" "+first_match.route,
+            "address_city" => first_match.city,
+            "address_state" => first_match.state_code,
+            "address_zip" => first_match.postal_code,
+            "address_country" => first_match.country_code,
+            "address" => params[:location][:address]
         }
     end
 
@@ -111,18 +156,9 @@ class LocationsController < ApplicationController
             "address_zip" => first_match.postal_code,
             "address_country" => first_match.country_code,
             "latitude" => first_match.latitude,
-            "longitude" => first_match.longitude
-
-            # TRIED BELOW FOR LAT/LONG... Needs fixing
-            # "latitude" => first_match.lat,
-            # "longitude" => first_match.lng
-            # "latitude" => params[:location][:latitude].to_f,
-            # "longitude" => params[:location][:longitude].to_f
-            # "country_full" => first_match.country,
-            # "address" => first_match.address
+            "longitude" => first_match.longitude,
+            "address" => params[:location][:address]
         }
-        # render json: street+", "+city+", "+state+", "+zip+", "+country
-        # render json: parsed_address_hash
     end
 
 end
