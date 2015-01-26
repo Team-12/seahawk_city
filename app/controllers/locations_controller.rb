2817480fbfa0196e1025b1906bf733a0131540ee
leviross <levross@gmail.com>
2015-01-25 22:47:38 -0800

    working on maps still

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -6,6 +6,7 @@ class LocationsController < ApplicationController
         @hash = Gmaps4rails.build_markers(@locations) do |location, marker|
             marker.lat location.latitude
             marker.lng location.longitude
+            marker.infowindow location.address
         end
     end
 
