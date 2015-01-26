31bedc8fc34fb890cb5d83f4276df17843b5a67e
Paul Howard <paul@paul-howard.net>
2015-01-25 19:05:31 -0800

    rake db:migrate, etc.

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20150126024506) do
-=======
-ActiveRecord::Schema.define(version: 20150126022604) do
->>>>>>> origin/bug_fix_latlng
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
