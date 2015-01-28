7520b458469ceaafbe9d47b8856f6b924df85c84
Paul Howard <paul@paul-howard.net>
2015-01-28 11:08:46 -0800

    Minor bug fixes

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,6 +1,10 @@
 class SiteController < ApplicationController
 
   def index
+
+    # If no session exists, render /about, else render /
+
+
     #made up html5 api gps static location
     @user_pin = [47.62326,-122.33025] # this will be replaced with gps from phone of user
     @last_user_checkin = Checkin.order("created_at").last
