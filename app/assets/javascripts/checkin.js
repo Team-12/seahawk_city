c9445a5286b38d71e7b12dc2f6307bf118e20b8c
Paul Howard <paul@paul-howard.net>
2015-01-28 15:10:57 -0800

    Add jQuery code to New Checkin form

--- app/assets/javascripts/checkin.js
+++ app/assets/javascripts/checkin.js
@@ -1,5 +1,17 @@
 $(function(){
-  $( ".nearby-location" ).click(function() {
-      alert("Click!");
+  
+  // Hide #newLocation div on page load
+  $( "#newLocation" ).hide();
+
+  // Show #newLocation div when needed
+  $( "#noneOfTheAbove" ).click(function() {
+    $( "#newLocation" ).toggle('slow');
   });
+
+  // Capture Checkinable id/type when existing value selected
+  $( ".nearby-location" ).click(function() {
+      $( "#c_id" ).val( $(this).data("id") );
+      $( "#c_type" ).val( $(this).data("type") );
+  });
+
 });
