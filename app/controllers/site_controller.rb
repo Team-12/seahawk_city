c5a209cb50bb3f0549b8611d96515e01558aab62
leviross <levross@gmail.com>
2015-01-28 11:31:38 -0800

    we now have green flags for events

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -6,10 +6,11 @@ class SiteController < ApplicationController
     @last_user_checkin = Checkin.order("created_at").last
     @locations = Location.all
     @events = Event.all
-    @hash = Gmaps4rails.build_markers(@locations) do |location, marker|
+    @location_hash = Gmaps4rails.build_markers(@locations) do |location, marker|
         marker.lat location.latitude
         marker.lng location.longitude
         #marker.infowindow gmaps4rails_infowindow
+
         marker.infowindow render_to_string(:partial => "/partials/marker", :locals => { :object => location })
         marker.picture({
             "url" => "https://s3.amazonaws.com/uploads.hipchat.com/39979/1426756/Gyhorx6vyPkjyto/12flags_blue.png",
@@ -17,6 +18,20 @@ class SiteController < ApplicationController
             "height" => 32
             })
     end
+
+       @event_hash = Gmaps4rails.build_markers(@events) do |event, marker|
+        marker.lat event.location.latitude
+        marker.lng event.location.longitude
+        #marker.infowindow gmaps4rails_infowindow
+        marker.infowindow render_to_string(:partial => "/partials/marker", :locals => { :object => event })
+        marker.picture({
+            "url" => "https://s3.amazonaws.com/uploads.hipchat.com/39979/1426756/cq3m4mJmN3WQHBL/12flags_green.png",
+            "width" => 32,
+            "height" => 32
+            })
+    end
+    @hash = @location_hash + @event_hash
+
   end
 
   def about
