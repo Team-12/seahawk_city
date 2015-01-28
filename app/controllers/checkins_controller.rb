17082b73d80d313927102c32278f281f6c675905
leviross <levross@gmail.com>
2015-01-28 15:09:10 -0800

    semi_final info window with event & checkin info

--- app/controllers/checkins_controller.rb
+++ app/controllers/checkins_controller.rb
@@ -32,14 +32,14 @@ class CheckinsController < ApplicationController
             new_location_id = Location.create({name: params[:checkin][:locations][:name], desc: params[:checkin][:locations][:desc], address_street: params[:checkin][:locations][:address_street], address_city: params[:checkin][:locations][:address_city],address_state: params[:checkin][:locations][:address_state], address_zip: params[:checkin][:locations][:address_zip], address_country: params[:checkin][:locations][:address_country],latitude: params[:checkin][:locations][:latitude], longitude: params[:checkin][:locations][:longitude]}).id
         end
 
-        result = capture_image params[:checkin][:photo].path
+        image_data = capture_image params[:checkin][:photo].path
         if new_location_id
             checkinable_id = new_location_id
         else
             checkinable_id = params[:checkin][:checkinable_id]
         end
-        @current_user.checkins << Checkin.create({photo_url: result['public_id'], note: params[:checkin][:note], latitude: params[:checkin][:latitude], longitude: params[:checkin][:longitude], checkinable_type: params[:checkin][:checkinable_type],checkinable_id: checkinable_id })
-        render json: result
+        @current_user.checkins << Checkin.create({photo_url: image_data['public_id'], note: params[:checkin][:note], latitude: params[:checkin][:latitude], longitude: params[:checkin][:longitude], checkinable_type: params[:checkin][:checkinable_type],checkinable_id: checkinable_id })
+        render json: image_data
 
 
     end
--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -3,37 +3,33 @@ class SiteController < ApplicationController
   def index
 
     # If no session exists, render /about, else render /
-
-
     #made up html5 api gps static location
     @user_pin = [47.62326,-122.33025] # this will be replaced with gps from phone of user
     @last_user_checkin = Checkin.order("created_at").last
     @locations = Location.all
     @events = Event.all
+    @checkins = Checkin.all
     @location_hash = Gmaps4rails.build_markers(@locations) do |location, marker|
-        marker.lat location.latitude
-        marker.lng location.longitude
-        #marker.infowindow gmaps4rails_infowindow
-
-        marker.infowindow render_to_string(:partial => "/partials/marker", :locals => { :object => location })
-        marker.picture({
-            "url" => "https://s3.amazonaws.com/uploads.hipchat.com/39979/1426756/Gyhorx6vyPkjyto/12flags_blue.png",
-            "width" => 32,
-            "height" => 32
-            })
+      marker.lat location.latitude
+      marker.lng location.longitude
+      marker.infowindow render_to_string(:partial => "/partials/marker", :locals => { :object => location })
+      marker.picture({
+        "url" => "https://s3.amazonaws.com/uploads.hipchat.com/39979/1426756/Gyhorx6vyPkjyto/12flags_blue.png",
+        "width" => 32,
+        "height" => 32
+        })
+    end
+    @event_hash = Gmaps4rails.build_markers(@events) do |event, marker|
+      marker.lat event.location.latitude
+      marker.lng event.location.longitude
+      marker.infowindow render_to_string(:partial => "/partials/marker", :locals => { :object => event })
+      marker.picture({
+        "url" => "https://s3.amazonaws.com/uploads.hipchat.com/39979/1426756/cq3m4mJmN3WQHBL/12flags_green.png",
+        "width" => 32,
+        "height" => 32
+        })
     end
 
-       @event_hash = Gmaps4rails.build_markers(@events) do |event, marker|
-        marker.lat event.location.latitude
-        marker.lng event.location.longitude
-        #marker.infowindow gmaps4rails_infowindow
-        marker.infowindow render_to_string(:partial => "/partials/marker", :locals => { :object => event })
-        marker.picture({
-            "url" => "https://s3.amazonaws.com/uploads.hipchat.com/39979/1426756/cq3m4mJmN3WQHBL/12flags_green.png",
-            "width" => 32,
-            "height" => 32
-            })
-    end
     @hash = @location_hash + @event_hash
 
   end
