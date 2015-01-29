4fd6f26ba73590f22e3184392f5af2e89d8da7a2
Paul Howard <paul@paul-howard.net>
2015-01-29 12:08:51 -0800

    Develop scripts for seeds.rb to populate db with clean data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,7 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 # Six Users
+p "--- Creating Users"
 users = User.create([
           { user_name: "SaintPete",
             email: "pete@seahawks.com",
@@ -39,8 +40,10 @@ users = User.create([
             password_confirmation: "123"
           }
         ])
+p "--- Created " + User.all.count.to_s + " users."
 
 # Locations
+p "--- Creating Locations"
 locations = Location.create([
           { user_id: 1,
             latitude: "47.6204",
@@ -64,8 +67,8 @@ locations = Location.create([
             address_zip: "98101",
             address_country: "USA"
           },
-          { latitude: nil,
-            longitude: nil,
+          { latitude: "47.597893",
+            longitude: "-122.332876",
             name: "Stadium Place Apartments",
             desc: "Close to the CLink",
             address_street: "521 Stadium Place South",
@@ -74,8 +77,8 @@ locations = Location.create([
             address_zip: "98104",
             address_country: "USA"
           },
-          { latitude: nil,
-            longitude: nil,
+          { latitude: "47.623284",
+            longitude: "-122.329997",
             name: "Ripple Yoga Studio",
             desc: "Right next door!",
             address_street: "1311 Republican Street",
@@ -95,8 +98,8 @@ locations = Location.create([
             address_country: "USA"
           },
           { user_id: nil,
-            latitude: nil,
-            longitude: nil,
+            latitude: "47.613455",
+            longitude: "-122.331670",
             name: "Paramount Theatre",
             desc: nil,
             address_street: "911 Pine Street",
@@ -108,8 +111,8 @@ locations = Location.create([
           },
           {
             user_id: nil,
-            latitude: nil,
-            longitude: nil,
+            latitude: "47.612096",
+            longitude: "-122.334270",
             name: "The Roosevelt Hotel",
             desc: nil,
             address_street: "1531 7th Avenue",
@@ -121,8 +124,8 @@ locations = Location.create([
           },
           {
             user_id: nil,
-            latitude: nil,
-            longitude: nil,
+            latitude: "47.617435",
+            longitude: "-122.344443",
             name: "City University of Seattle",
             desc: nil,
             address_street: "521 Wall Street",
@@ -134,8 +137,8 @@ locations = Location.create([
           },
           {
             user_id: nil,
-            latitude: nil,
-            longitude: nil,
+            latitude: "47.607452",
+            longitude: "-122.342975",
             name: "Seattle Aquarium",
             desc: nil,
             address_street: "1483 Alaskan Way",
@@ -147,21 +150,8 @@ locations = Location.create([
           },
           {
             user_id: nil,
-            latitude: nil,
-            longitude: nil,
-            name: "The Seattle Great Wheel",
-            desc: nil,
-            address_street: "1301 Alaskan Way",
-            address_city: "Seattle" ,
-            address_state: "WA",
-            address_zip: "98101",
-            website: "seattlegreatwheel.com",
-            phone_num: "(206) 623-8607"
-          },
-          {
-            user_id: nil,
-            latitude: nil,
-            longitude: nil,
+            latitude: "47.608246",
+            longitude: "-122.339727",
             name: "The Pike Brewing Company",
             desc: nil,
             address_street: "1415 1st Avenue",
@@ -172,8 +162,8 @@ locations = Location.create([
           },
           {
             user_id: nil,
-            latitude: nil,
-            longitude: nil,
+            latitude: "47.608110",
+            longitude: "-122.336965",
             name: "Benaroya Hall",
             desc: nil,
             address_street: "200 University Street",
@@ -185,8 +175,8 @@ locations = Location.create([
           },
           {
             user_id: nil,
-            latitude: nil,
-            longitude: nil,
+            latitude: "47.593294",
+            longitude: "-122.333651",
             name: "The Hawks Nest",
             desc: nil,
             address_street: "1028 1st Avenue South",
@@ -198,8 +188,8 @@ locations = Location.create([
           },
           {
             user_id: nil,
-            latitude: nil,
-            longitude: nil,
+            latitude: "47.621629",
+            longitude: "-122.348246",
             name: "EMP Museum",
             desc: nil,
             address_street: "325 5th Avenue North",
@@ -211,8 +201,8 @@ locations = Location.create([
           },
           {
             user_id: nil,
-            latitude: nil,
-            longitude: nil,
+            latitude: "47.676174",
+            longitude: "-122.317592",
             name: "Rain City Burgers",
             desc: nil,
             address_street: "6501 Roosevelt Way Northeast",
@@ -224,8 +214,8 @@ locations = Location.create([
           },
           {
             user_id: nil,
-            latitude: nil,
-            longitude: nil,
+            latitude: "47.576827",
+            longitude: "-122.167585",
             name: "T-Mobile Headquarters",
             desc: nil,
             address_street: "12920 SE 38th Street",
@@ -237,8 +227,8 @@ locations = Location.create([
           },
           {
             user_id: nil,
-            latitude: nil,
-            longitude: nil,
+            latitude: "47.603734",
+            longitude: "-122.323103",
             name: "Harborview Medical Center",
             desc: nil,
             address_street: "325 9th Avenue",
@@ -250,8 +240,8 @@ locations = Location.create([
           },
           {
             user_id: nil,
-            latitude: nil,
-            longitude: nil,
+            latitude: "47.606313",
+            longitude: "-122.341926",
             name: "The Seattle Great Wheel",
             desc: nil,
             address_street: "1301 Alaskan Way",
@@ -263,8 +253,8 @@ locations = Location.create([
           },
           {
             user_id: nil,
-            latitude: nil,
-            longitude: nil,
+            latitude: "47.580945",
+            longitude: "-122.335935",
             name: "Starbucks Headquarters",
             desc: nil,
             address_street: "2401 Utah Avenue South",
@@ -276,8 +266,8 @@ locations = Location.create([
           },
           {
             user_id: nil,
-            latitude: nil,
-            longitude: nil,
+            latitude: "47.649534",
+            longitude: "-122.352026",
             name: "Allen Institute for Brain Science",
             desc: nil,
             address_street: "551 North 34th Street #200",
@@ -289,8 +279,8 @@ locations = Location.create([
           },
           {
             user_id: nil,
-            latitude: nil,
-            longitude: nil,
+            latitude: "47.568323",
+            longitude: "-122.364321",
             name: "Metropolitan Market Retail Support Center",
             desc: nil,
             address_street: "4025 Delridge Way Southwest #100",
@@ -302,8 +292,8 @@ locations = Location.create([
           },
           {
             user_id: nil,
-            latitude: nil,
-            longitude: nil,
+            latitude: "47.602060",
+            longitude: "-122.331770",
             name: "Smith Tower",
             desc: nil,
             address_street: "506 2nd Avenue",
@@ -314,8 +304,10 @@ locations = Location.create([
             phone_num: "(206) 622-4004"
           }            
         ])
+p "--- Created " + Location.all.count.to_s + " locations."
 
 # Six Events
+p "--- Creating Events"
 events = Event.create([
           { user_id: User.first.id,
             location_id: Location.first.id,
@@ -329,23 +321,50 @@ events = Event.create([
         ])
 
 # Add Events to about 30% of Locations
-locations.each do |l|
-  i = rand(4)
-  while i = 0  
+locations.each do |loc|
+  i = rand(1..4)
+  while i == 1  
     Event.create([
-          { user_id: 
-            location_id: l.id,
+          { user_id: nil,
+            location_id: loc.id,
             name: "Seahawks Party",
             desc: "A 12th Man Celebration",
-            date: "2015/02/01",
+            date: "2015/02/" + rand(1...28).to_s,
             start_time: "12:00",
             end_time: "8:00",
-            photo_url: "http://media.thenewstribune.com/smedia/2014/05/08/23/00/1wC2dn.AuSt.5.jpg"
+            photo_url: "" # Need cloudinary photo ids here...
           }
         ])
-    i = rand(4-1)+1
+    i = rand(1..2) # Increase odds of Location with at least 1 to have others.
   end
 end
-
+p "--- Created " + Event.all.count.to_s + " events."
 
 # For each user, create 5-15 checkins for random Locations/Events
+p "--- Creating Checkins"
+users.each do |user|
+  i = rand(5..15)
+  count = 1
+  while i < 20
+
+    # (2:1 ratio of location checkins to event checkins)
+    if rand(1..3) < 3
+      checkinable_type = "location"
+      checkinable = Location.find_by_id(rand(1..Location.all.length))
+    else
+      checkinable_type = "event"
+      checkinable = Event.find_by_id(rand(1..Event.all.length))
+    end
+
+    note = "Test Checkin #" + count.to_s + " for " + user.user_name
+    latitude = checkinable_type == "location" ? checkinable.latitude : checkinable.location.latitude
+    longitude = checkinable_type == "location" ? checkinable.longitude : checkinable.location.longitude
+
+    # Create the checkin
+    user.checkins << checkinable.checkins.create({photo_url: "", note: note, latitude: latitude, longitude: longitude})
+
+    i += 1
+    count += 1
+  end
+end
+p "--- Created " + Checkin.all.count.to_s + " checkins."
