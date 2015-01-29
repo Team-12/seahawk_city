938d85c91a16b7727a55b150fb094481a288a128
leviross <levross@gmail.com>
2015-01-29 11:01:52 -0800

    still cant save location id for new event

--- app/assets/javascripts/profile.js
+++ app/assets/javascripts/profile.js
@@ -1,19 +1,7 @@
 $(function(){
-  // I AM GOING TO ADAPT THIS FOR PASSING LOCATON ID TO SAVE FOR NEW EVENT
-  // Hide #newLocation div on page load
-  //$( "#newLocation" ).hide();
+    var locationParams = $('#location-id');
+    locationParams.val =
 
-  // Capture Checkinable id/type when existing value selected
-  $( ".nearby-location" ).click(function() {
-      $( "#c_id" ).val( $(this).data("id") );
-      $( "#c_type" ).val( $(this).data("type") );
-  });
-
-  // Show #newLocation div when needed
-  $( "#noneOfTheAbove" ).click(function() {
-    $( "#newLocation" ).toggle('slow');
-    $( "#c_type" ).val( "new" );
-  });
 
 
 });
